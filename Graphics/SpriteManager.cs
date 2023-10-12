e81ec1291e3c0fdd251d5ff5cce50fccd6c5a4a3
Edmond Ma <edmondma@live.com>
2023-10-11 20:32:44 -0400

    Documented SpriteManager.

--- Graphics/SpriteManager.cs
+++ Graphics/SpriteManager.cs
@@ -24,10 +24,14 @@ namespace Arcadia.Graphics
             Instance ??= new SpriteManager(game);
         }
 
+        /// <summary>
+        /// Begins a new sprite and text batch with the specified render state.
+        /// </summary>
+        /// <param name="camera">The camera.</param>
+        /// <param name="isTextureFilteringEnabled">Is texture filtering enabled?</param>
         public static void Begin(Camera camera, bool isTextureFilteringEnabled)
         {
-            SamplerState samplerState = (isTextureFilteringEnabled)
-                ? SamplerState.LinearClamp : SamplerState.PointClamp;
+            SamplerState samplerState = (isTextureFilteringEnabled) ? SamplerState.LinearClamp : SamplerState.PointClamp;
 
             if (camera is null)
             {
@@ -47,40 +51,108 @@ namespace Arcadia.Graphics
             Instance._spriteBatch.Begin(blendState: BlendState.AlphaBlend, samplerState: samplerState, rasterizerState: RasterizerState.CullNone, effect: Instance._basicEffect);
         }
 
+        /// <summary>
+        /// Begins a new sprite and text batch with the specified render state.
+        /// </summary>
         public static void Begin() => Instance._spriteBatch.Begin();
 
+        /// <summary>
+        /// Flushes all batched text and sprites to the screen.
+        /// </summary>
+        /// <remarks>This command should be called after <see cref="Begin()"/> or <see cref="Begin(Camera, bool)"/> and drawing commands.</remarks>
         public static void End() => Instance._spriteBatch.End();
 
+        /// <summary>
+        /// Submit a sprite for drawing in the current batch.
+        /// </summary>
+        /// <param name="texture">A texture.</param>
+        /// <param name="destinationRectangle">The drawing bounds on screen.</param>
+        /// <param name="color">A color mask.</param>
         public static void Draw(Texture2D texture, Rectangle destinationRectangle, Color color)
         {
             Instance._spriteBatch.Draw(texture, destinationRectangle, color);
         }
 
+        /// <summary>
+        /// Submit a sprite for drawing in the current batch.
+        /// </summary>
+        /// <param name="texture">A texture.</param>
+        /// <param name="destinationRectangle">The drawing bounds on screen.</param>
+        /// <param name="sourceRectangle">An optional region on the texture which will be rendered. If null - draws full texture.</param>
+        /// <param name="color">A color mask.</param>
         public static void Draw(Texture2D texture, Rectangle destinationRectangle, Rectangle? sourceRectangle, Color color)
         {
             Instance._spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, color);
         }
 
+        /// <summary>
+        /// Submit a sprite for drawing in the current batch.
+        /// </summary>
+        /// <param name="texture">A texture.</param>
+        /// <param name="destinationRectangle">The drawing bounds on screen.</param>
+        /// <param name="sourceRectangle">An optional region on the texture which will be rendered. If null - draws full texture.</param>
+        /// <param name="color">A color mask.</param>
+        /// <param name="rotation">A rotation of this sprite.</param>
+        /// <param name="origin">Center of the rotation. 0,0 by default.</param>
+        /// <param name="effects">Modificators for drawing. Can be combined.</param>
+        /// <param name="layerDepth">A depth of the layer of this sprite.</param>
         public static void Draw(Texture2D texture, Rectangle destinationRectangle, Rectangle? sourceRectangle, Color color, float rotation, Vector2 origin, SpriteEffects effects, float layerDepth)
         {
             Instance._spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, color, rotation, origin, effects, layerDepth);
         }
 
+        /// <summary>
+        /// Submit a sprite for drawing in the current batch.
+        /// </summary>
+        /// <param name="texture">A texture.</param>
+        /// <param name="position">The drawing location on screen.</param>
+        /// <param name="color">A color mask.</param>
         public static void Draw(Texture2D texture, Vector2 position, Color color)
         {
             Instance._spriteBatch.Draw(texture, position, color);
         }
 
+        /// <summary>
+        /// Submit a sprite for drawing in the current batch.
+        /// </summary>
+        /// <param name="texture">A texture.</param>
+        /// <param name="position">The drawing location on screen.</param>
+        /// <param name="sourceRectangle">An optional region on the texture which will be rendered. If null - draws full texture.</param>
+        /// <param name="color">A color mask.</param>
         public static void Draw(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color)
         {
             Instance._spriteBatch.Draw(texture, position, sourceRectangle, color);
         }
 
+        /// <summary>
+        /// Submit a sprite for drawing in the current batch.
+        /// </summary>
+        /// <param name="texture">A texture.</param>
+        /// <param name="position">The drawing location on screen.</param>
+        /// <param name="sourceRectangle">An optional region on the texture which will be rendered. If null - draws full texture.</param>
+        /// <param name="color">A color mask.</param>
+        /// <param name="rotation">A rotation of this sprite.</param>
+        /// <param name="origin">Center of the rotation. 0,0 by default.</param>
+        /// <param name="scale">A scaling of this sprite.</param>
+        /// <param name="effects">Modificators for drawing. Can be combined.</param>
+        /// <param name="layerDepth">A depth of the layer of this sprite.</param>
         public static void Draw(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color, float rotation, Vector2 origin, Vector2 scale, SpriteEffects effects, float layerDepth)
         {
             Instance._spriteBatch.Draw(texture, position, sourceRectangle, color, rotation, origin, scale, effects, layerDepth);
         }
 
+        /// <summary>
+        /// Submit a sprite for drawing in the current batch.
+        /// </summary>
+        /// <param name="texture">A texture.</param>
+        /// <param name="position">The drawing location on screen.</param>
+        /// <param name="sourceRectangle">An optional region on the texture which will be rendered. If null - draws full texture.</param>
+        /// <param name="color">A color mask.</param>
+        /// <param name="rotation">A rotation of this sprite.</param>
+        /// <param name="origin">Center of the rotation. 0,0 by default.</param>
+        /// <param name="scale">A scaling of this sprite.</param>
+        /// <param name="effects">Modificators for drawing. Can be combined.</param>
+        /// <param name="layerDepth">A depth of the layer of this sprite.</param>
         public static void Draw(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth)
         {
             Instance._spriteBatch.Draw(texture, position, sourceRectangle, color, rotation, origin, scale, effects, layerDepth);
