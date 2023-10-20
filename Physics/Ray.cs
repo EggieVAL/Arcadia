1cf1993ff3a732112ee9e09cb23bca1cab3201d0
Kris TG <kketg03@gmail.com>
2023-10-20 16:56:22 -0400

    Added Ray Class and RayHit struct

---
+++ Physics/Ray.cs
@@ -0,0 +1,49 @@
+﻿using Arcadia.GameObjects;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcadia.Physics
+{
+    public class Ray
+    {
+        public Vector2 Start;
+        public Vector2 Direction;
+        public float Length = 250f;
+        public bool Collided = false;
+
+        public Ray(Vector2 start, Vector2 dir)
+        {
+            Start = start;
+            Direction = dir;
+        }
+        public Ray(Vector2 start, Vector2 dir, float length)
+        {
+            Start = start;
+            Direction = dir;
+            Length = length;
+        }
+
+        public RayHit Cast()
+        {
+            return new RayHit();
+        }
+
+    }
+
+    public struct RayHit
+    {
+        public Vector2 point;
+        public RenderableObject obj;
+
+        public RayHit(Vector2 point, RenderableObject obj)
+        {
+            this.point = point;
+            this.obj = obj;
+        }
+    }
+}
+
