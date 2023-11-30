4c1c798ce5a166111327447ff564f44e9cef6ab8
Kris TG <kketg03@gmail.com>
2023-11-30 16:44:32 -0500

    Reimplemented PlayerClasses and CharacterStats

---
+++ GameObjects/Characters/CharacterStats.cs
@@ -0,0 +1,111 @@
+﻿using Arcadia.GameObjects.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcadia.GameObjects.Characters
+{
+    public class CharacterStats
+    {
+        float _maxHP;
+        float _HP;
+
+        float _maxMP;
+        float _MP;
+
+        float _maxSP;
+        float _SP;
+
+        public int VIT { get; set; }
+        public int STR { get; set; }
+        public int DEF { get; set; }
+        public int MAG { get; set; }
+        public int AGI { get; set; }
+        public int DEX { get; set; }
+        public int INT { get; set; }
+        public int SLA { get; set; }
+
+        public CharacterStats()
+        {
+            VIT = 5;
+            STR = 5;
+            DEF = 5;
+            MAG = 5;
+            AGI = 5;
+            DEX = 5;
+            INT = 5;
+            SLA = 5;
+            InitPointStats();
+        }
+
+        public CharacterStats(PlayerClass playerClass)
+        {
+            switch (playerClass)
+            {
+                case PlayerClass.Warrior:
+                    VIT = 10;
+                    STR = 10;
+                    DEF = 10;
+                    MAG = 1;
+                    AGI = 1;
+                    DEX = 5;
+                    INT = 1;
+                    SLA = 5;
+                    break;
+                case PlayerClass.Hunter:
+                    VIT = 1;
+                    STR = 5;
+                    DEF = 1;
+                    MAG = 1;
+                    AGI = 10;
+                    DEX = 10;
+                    INT = 10;
+                    SLA = 5;
+                    break;
+                case PlayerClass.Rogue:
+                    VIT = 1;
+                    STR = 1;
+                    DEF = 5;
+                    MAG = 1;
+                    AGI = 10;
+                    DEX = 10;
+                    INT = 5;
+                    SLA = 10;
+                    break;
+                case PlayerClass.Mage:
+                    VIT = 5;
+                    STR = 1;
+                    DEF = 10;
+                    MAG = 10;
+                    AGI = 1;
+                    DEX = 1;
+                    INT = 5;
+                    SLA = 10;
+                    break;
+                default:
+                    VIT = 5;
+                    STR = 5;
+                    DEF = 5;
+                    MAG = 5;
+                    AGI = 5;
+                    DEX = 5;
+                    INT = 5;
+                    SLA = 5;
+                    break;
+            }
+            InitPointStats();
+        }
+
+        public void InitPointStats()
+        {
+            _maxHP = 50 + (VIT * 5);
+            _maxMP = 50 + (MAG * 5);
+            _maxSP = 50 + (AGI * 5);
+            _HP = _maxHP;
+            _MP = _maxMP;
+            _SP = _maxSP;
+        }
+    }
+}
--- GameObjects/Characters/Player.cs
+++ GameObjects/Characters/Player.cs
@@ -15,14 +15,27 @@ namespace Arcadia.GameObjects.Characters
     /// </summary>
     public sealed class Player : Character
     {
+        PlayerClass _class { get; set; }
+        CharacterStats _stats { get; set; }
+
+        float abilityCooldown;
+        float abilityCooldownTimer;
+
+        World _world;
         /// <summary>
         /// Constructs a player.
         /// </summary>
         /// <param name="texture">The texture of a player.</param>
         /// <param name="bounds">The bounds of a player.</param>
         /// <param name="world">The world a player is in.</param>
-        public Player(Texture2D texture, Rectangle bounds, World world) : base(texture, bounds, world)
+        public Player(PlayerClass playerClass, Texture2D[] texture, Rectangle bounds, World world) : base(texture, bounds, world)
         {
+            _class = playerClass;
+            _stats = new CharacterStats(playerClass);
+            _world = world;
+
+            abilityCooldown = 1;
+            abilityCooldownTimer = abilityCooldown;
         }
 
         /// <summary>
@@ -31,23 +44,67 @@ namespace Arcadia.GameObjects.Characters
         /// <param name="gameTime"></param>
         public override void Update(GameTime gameTime)
         {
+            float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float currentX = X;
+            float currentY = Y;
+            float nextX = (X += VelocityX * elapsedTime);
+            float nextY = (Y += VelocityY * elapsedTime);
+
             VelocityX = 0f;
             VelocityY += 0.0015265f * Grid.Size;
 
+            if (abilityCooldownTimer > 0)
+            {
+                abilityCooldownTimer -= elapsedTime;
+            }
+
+
             if (KeyListener.IsKeyPressed(Keys.A))
             {
-                VelocityX = -0.2f;
+                VelocityX = -(0.2f + _stats.AGI * 0.05f);
             }
             if (KeyListener.IsKeyPressed(Keys.D))
             {
-                VelocityX = 0.2f;
+                VelocityX = 0.2f + _stats.AGI * 0.05f;
             }
             if (KeyListener.IsKeyPressed(Keys.LeftShift))
             {
-                VelocityX *= 2;
+                VelocityX *= 2 + _stats.AGI * 0.1f;
+            }
+
+            if (KeyListener.IsKeyPressed(Keys.E))
+            {
+                if (abilityCooldownTimer <= 0)
+                {
+                    Vector2 worldPos = _world.GetMousePosition();
+                    Vector2 dir = new Vector2(worldPos.X - this.X, worldPos.Y - this.Y);
+
+                    dir.Normalize();
+                    dir *= 3 + (_stats.DEX / 10);
+                    _world.CreateProjectile(0, (int)X, (int)(Y - 2), dir.X, dir.Y);
+                    abilityCooldownTimer = abilityCooldown;
+                }
+            }
+
+            bool grounded = IsCollidingBelow(currentY, nextY, out List<Tile> tilesCollided);
+            if (KeyListener.IsKeyPressed(Keys.Space))
+            {
+                if (grounded)
+                {
+                    VelocityY -= (0.75f + _stats.AGI * 0.1f);
+                }
+
             }
 
             base.Update(gameTime);
         }
+    }
+
+    public enum PlayerClass
+    {
+        Warrior = 0,
+        Hunter = 1,
+        Rogue = 2,
+        Mage = 3
     }
 }
